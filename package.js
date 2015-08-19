3ebd7f4b4e973874fcd1ca85ebf028dff73510c1
Rod Cope <rod@perfdog.com>
2015-08-18 18:29:21 -0600

    Changed package name to reflect new home.

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   summary: "Nicely formatted dropdown list of timezones, based on moment. Also timezone detection.",
   version: '0.1.2',
-  name: "joshowens:timezone-picker",
-  git: 'https://github.com/MeteorClub/timezone-picker'
+  name: "rodcope:timezone-picker",
+  git: 'https://github.com/rodcope1/timezone-picker'
 });
 
 Package.onUse(function(api, where) {
