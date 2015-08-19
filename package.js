80b589be1c45ef0683a55e4793c4d4e8b6c76581
Rod Cope <rod@perfdog.com>
2015-08-19 07:42:03 -0600

    Export TimeZonePicker with a capital 'Z'.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Nicely formatted dropdown list of timezones, based on moment. Also timezone detection.",
-  version: '0.1.6',
+  version: '0.1.7',
   name: "rodcope:timezone-picker",
   git: 'https://github.com/rodcope1/timezone-picker'
 });
